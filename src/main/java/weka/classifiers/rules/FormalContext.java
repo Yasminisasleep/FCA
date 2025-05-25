d05fd15ab436f095222c89fe54b280c7960ac19f
Yasmine Benali <yasmine.benali@epita.fr>
2025-05-25 19:17:46 +0200

    GRAND 63%

--- src/main/java/weka/classifiers/rules/FormalContext.java
+++ src/main/java/weka/classifiers/rules/FormalContext.java
@@ -1,51 +1,77 @@
 package weka.classifiers.rules;
 
+import weka.core.Instance;
 import weka.core.Instances;
-import java.util.HashSet;
-import java.util.Set;
+
+import java.util.*;
 
 public class FormalContext {
-    private final boolean[][] context;
-    private final int objectCount;
-    private final int attributeCount;
 
-    public FormalContext(boolean[][] context) {
-        this.context = context;
-        this.objectCount = context.length;
-        this.attributeCount = context[0].length;
+    private final Instances data;
+
+    public FormalContext(Instances data) {
+        this.data = data;
     }
 
     public static FormalContext fromInstances(Instances data) {
-        int rows = data.numInstances();
-        int cols = data.numAttributes() - 1;
-
-        boolean[][] ctx = new boolean[rows][cols];
-        for (int i = 0; i < rows; i++)
-            for (int j = 0; j < cols; j++)
-                ctx[i][j] = data.instance(i).value(j) > 0;
-
-        return new FormalContext(ctx);
-    }
-
-    public Set<Integer> commonAttributes(Set<Integer> objects) {
-        Set<Integer> common = new HashSet<>();
-        for (int attr = 0; attr < attributeCount; attr++) {
-            boolean shared = true;
-            for (int obj : objects)
-                if (!context[obj][attr]) {
-                    shared = false;
-                    break;
-                }
-            if (shared) common.add(attr);
-        }
-        return common;
-    }
-
-    public int getObjectCount() {
-        return objectCount;
+        return new FormalContext(data);
     }
 
     public int getAttributeCount() {
-        return attributeCount;
+        // Exclure l'attribut de classe
+        return data.numAttributes() - 1;
     }
+
+    public Instance getInstance(int index) {
+        return data.instance(index);
+    }
+
+    public int getObjectCount() {
+        return data.numInstances();
+    }
+
+    public List<FormalConcept> getAllConcepts() {
+        return new ArrayList<>();
+    }
+
+    public boolean[] getIntent(Instance instance) {
+        boolean[] intent = new boolean[getAttributeCount()];
+        for (int i = 0, j = 0; i < data.numAttributes(); i++) {
+            if (i == data.classIndex()) continue;
+            intent[j++] = !instance.isMissing(i) && instance.stringValue(i).equals("yes");
+        }
+        return intent;
+    }
+
+    public double getClassValue(Instance instance) {
+        return instance.classValue();
+    }
+    public Set<Integer> commonAttributes(Set<Integer> objectIndices) {
+        Set<Integer> common = new HashSet<>();
+
+        if (objectIndices.isEmpty()) return common;
+
+        // Initialiser tous à true au début
+        boolean[] commonFlags = new boolean[getAttributeCount()];
+        Arrays.fill(commonFlags, true);
+
+        for (int index : objectIndices) {
+            Instance inst = getInstance(index);
+            boolean[] intent = getIntent(inst);
+
+            for (int i = 0; i < commonFlags.length; i++) {
+                commonFlags[i] &= intent[i]; // intersection logique
+            }
+        }
+
+        // Convertir le tableau booléen en Set d'indices
+        for (int i = 0; i < commonFlags.length; i++) {
+            if (commonFlags[i]) {
+                common.add(i);
+            }
+        }
+
+        return common;
+    }
+
 }
--- src/main/java/weka/classifiers/rules/GrandRuleExtractor.java
+++ src/main/java/weka/classifiers/rules/GrandRuleExtractor.java
@@ -3,11 +3,12 @@ package weka.classifiers.rules;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.*;
+import weka.core.Instance;
 
 public class GrandRuleExtractor {
-
-    private final ConceptLattice lattice;
     private final FormalContext context;
+    private final ConceptLattice lattice;
 
     public GrandRuleExtractor(ConceptLattice lattice, FormalContext context) {
         this.lattice = lattice;
@@ -27,11 +28,22 @@ public class GrandRuleExtractor {
             double predictedClass = determineClass(concept.getExtent());
             rules.add(new GrandRule(conditions, predictedClass));
         }
+
         return rules;
     }
 
     private double determineClass(Set<Integer> extent) {
-        // Adapte avec ta vraie logique
-        return extent.size() % 2;
+        Map<Double, Integer> classCounts = new HashMap<>();
+
+        for (Integer i : extent) {
+            Instance inst = context.getInstance(i);
+            double classVal = inst.classValue();
+            classCounts.put(classVal, classCounts.getOrDefault(classVal, 0) + 1);
+        }
+
+        return classCounts.entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(0.0);
     }
 }
